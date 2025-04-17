f64d0c3a0435aa93a1ef14ace26527cc42a8a954
Dassana <dassana002@gmail.com>
2025-04-17 15:00:21 +0530

    Init responses

--- introduction/src/routes/BookRoutes.js
+++ introduction/src/routes/BookRoutes.js
@@ -6,19 +6,39 @@ const BookService = require("../services/BookService")
 
 router.get(bookUrl, async (req, res) => {
     // controll a get request
-    await BookService.getAllBooks();
+    try{
+        await BookService.getAllBooks();
+        res.status(200).send("Get Books!!")
+    }catch(er){
+        console.error(er)
+    }
 });
 
 router.post(bookUrl, async (req, res) => {
-    await BookService.addBook();
+    try{
+        await BookService.addBook();
+        res.status(201).send("Add Book!!")
+    }catch(er){
+        console.error(er)
+    }
 });
 
 router.patch(bookUrl, async (req, res) => {
-    await BookService.updateBook();
+    try{
+        await BookService.updateBook();
+        res.status(204).send("Update Book!!")
+    }catch(er){
+        console.error(er)
+    }
 });
 
 router.delete(bookUrl, async (req, res) => {
-    await BookService.deleteBook();
+    try{
+        await BookService.deleteBook();
+        res.status(204).send("Delete Book!!")
+    }catch(er){
+        console.error(er)
+    }
 });
 
 module.exports = router;
