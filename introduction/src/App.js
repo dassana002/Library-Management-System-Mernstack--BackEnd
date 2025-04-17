c44c0ca18684fba2727389282c673d9ba7813b2a
Dassana <dassana002@gmail.com>
2025-04-17 13:35:35 +0530

    Express JS installing

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -1,11 +1,11 @@
 const http = require("http")
- const PORT = 3500
- 
- const server = http.createServer((req,res)=>{
-     res.writeHead(200,{"Content-Type":"text/plain"})
-     res.end("Hello MERN - CMJD 109")
- });
- 
- server.listen(PORT,()=>{
-     console.log(`Server Started PORT: ${PORT} `)
- })
+const PORT = 3500
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end("Hello MERN - CMJD 109")
+});
+
+server.listen(PORT, () => {
+    console.log(`Server Started PORT: ${PORT} `)
+})
